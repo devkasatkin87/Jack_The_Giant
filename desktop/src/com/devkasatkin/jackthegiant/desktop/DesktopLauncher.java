b98eed8f3a6a98ae8d58523e9510337187050d78
kasatkin <devkasatkin87@gmail.com>
2021-02-08 18:26:59 +0200

    Added helpers with GameInfo. Set screen width and height.

--- desktop/src/com/devkasatkin/jackthegiant/desktop/DesktopLauncher.java
+++ desktop/src/com/devkasatkin/jackthegiant/desktop/DesktopLauncher.java
@@ -3,10 +3,14 @@ package com.devkasatkin.jackthegiant.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.devkasatkin.jackthegiant.GameMain;
+import helpers.GameInfo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = GameInfo.WIDTH;
+		config.height = GameInfo.HEIGHT;
+		config.title = "Jack The Giant";
 		new LwjglApplication(new GameMain(), config);
 	}
 }
