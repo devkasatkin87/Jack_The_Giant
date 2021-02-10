13b2147587f2abc6213302435cb96f1f5040bdf3
kasatkin <devkasatkin87@gmail.com>
2021-02-10 20:29:54 +0200

    Added main camera and viewport.

--- core/src/com/devkasatkin/jackthegiant/scenes/Gameplay.java
+++ core/src/com/devkasatkin/jackthegiant/scenes/Gameplay.java
@@ -3,17 +3,37 @@ package com.devkasatkin.jackthegiant.scenes;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.devkasatkin.jackthegiant.helpers.GameInfo;
 import com.devkasatkin.jackthegiant.main.GameMain;
 
 public class Gameplay implements Screen {
     private final GameMain game;
     private Sprite[] bgs;
+    private OrthographicCamera mainCamera;
+    private Viewport viewport;
 
     public Gameplay(GameMain game) {
         this.game = game;
+
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
+
+        viewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, mainCamera);
+
         createBackgrounds();
+    }
+
+    public void update(float dt) {
+        moveCamera();
+    }
+
+    private void moveCamera() {
+        mainCamera.position.y -= 1;
     }
 
 
@@ -26,7 +46,7 @@ public class Gameplay implements Screen {
     }
 
     private void drawBackgrounds() {
-        for (Sprite bg : bgs) {
+         for (Sprite bg : bgs) {
             game.getBatch().draw(bg, bg.getX(), bg.getY());
         }
     }
@@ -38,11 +58,17 @@ public class Gameplay implements Screen {
 
     @Override
     public void render(float delta) {
+
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.getBatch().begin();
         drawBackgrounds();
         game.getBatch().end();
+
+        game.getBatch().setProjectionMatrix(mainCamera.combined);
+        mainCamera.update();
     }
 
     @Override
